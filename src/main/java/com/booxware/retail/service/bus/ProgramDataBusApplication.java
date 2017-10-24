2bce7a54db1636cc3164527540101ce091551fb0
Cristian Busoi <cristian.busoi@booxware.de>
2017-10-24 10:33:31 +0200

    set cache to redis

--- src/main/java/com/booxware/retail/service/bus/ProgramDataBusApplication.java
+++ src/main/java/com/booxware/retail/service/bus/ProgramDataBusApplication.java
@@ -4,30 +4,26 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.concurrent.Callable;
-
-import javax.annotation.PostConstruct;
 
 import org.apache.commons.jxpath.JXPathContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.support.collections.DefaultRedisMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -65,42 +61,39 @@ import com.fasterxml.jackson.databind.JsonNode;
 @SpringBootApplication
 @RestController
 @EnableCaching
-@DependsOn("cacheManager")
 public class ProgramDataBusApplication {
 
-	private static final int CACHE_CAPACITY = 100;
-	private static final int CACHE_TIMEOUT = 10000;
 	private static final int HTTP_CONNECTION_TIMEOUT = 1000000;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramDataBusApplication.class, args);
 	}
 
-	// @Bean
-	// @Profile("guava")
-	// CacheBuilderSpec cacheBuilderSpec() {
-	// return CacheBuilderSpec
-	// .parse(String.format("maximumSize=?,expireAfterAccess=?", CACHE_CAPACITY,
-	// CACHE_TIMEOUT));
-	// }
-	//
-	@Autowired
-	CacheManager cacheManager;
-
-	ListableCache eventsCache;
-	Map<SseEmitter, String> subscribersAndFilters;
-
-	@PostConstruct
-	public void setup() {
-		this.eventsCache = ListableCache.build(cacheManager.getCache("events"));
-		this.subscribersAndFilters = new HashMap<>();
+	@Bean
+	AsyncSupportConfigurer configurer() {
+		AsyncSupportConfigurer configurer = new AsyncSupportConfigurer();
+		configurer.setDefaultTimeout(HTTP_CONNECTION_TIMEOUT);
+		return configurer;
+	}
+	
+	@Bean
+	RedisCacheManager cacheManager(StringRedisTemplate template) {
+		return new RedisCacheManager(template);
 	}
 
+	Map<Object,Object> eventsCache;
+	Map<SseEmitter, String> subscribersAndFilters;
+
+	public ProgramDataBusApplication(StringRedisTemplate template) {
+		this.eventsCache = new DefaultRedisMap<Object,Object> ("events", template);
+		this.subscribersAndFilters = new HashMap<>();
+	}
+	
 	@GetMapping(path = "/events", produces = "text/event-stream")
 	public SseEmitter subscribe(@RequestParam(name = "query", defaultValue = "") String xpath) {
 
 		SseEmitter emitter = new SseEmitter();
-		filterAndSend(emitter, eventsCache.keys(), xpath);
+		filterAndSend(emitter, eventsCache.keySet(), xpath);
 
 		subscribersAndFilters.put(emitter, xpath);
 		Runnable remove = () -> {
@@ -139,7 +132,7 @@ public class ProgramDataBusApplication {
 	}
 
 	private void sendToSubscribers(Object event) {
-		eventsCache.put(event.toString(), event);
+		eventsCache.put(event, event);
 
 		List<?> eventList = Arrays.asList(event);
 		for (Entry<SseEmitter, String> entry : subscribersAndFilters.entrySet()) {
@@ -154,77 +147,3 @@ public class ProgramDataBusApplication {
 	}
 }
 
-class ListableCache implements Cache {
-	private final Set<Object> keys;
-	private final Cache cache;
-
-	public static ListableCache build(Cache cache) {
-		return new ListableCache(cache);
-	}
-
-	private ListableCache(Cache cache) {
-		this.cache = cache;
-		keys = new HashSet<>();
-	}
-
-	public Set<Object> keys() {
-		return keys;
-	}
-
-	@Override
-	public void clear() {
-		cache.clear();
-		keys.clear();
-	}
-
-	@Override
-	public void evict(Object arg0) {
-		cache.evict(arg0);
-		keys.remove(arg0);
-	}
-
-	@Override
-	public <T> T get(Object arg0, Callable<T> arg1) {
-		keys.add(arg0);
-		return cache.get(arg0, arg1);
-	}
-
-	@Override
-	public <T> T get(Object arg0, Class<T> arg1) {
-		keys.add(arg0);
-		return cache.get(arg0, arg1);
-	}
-
-	@Override
-	public ValueWrapper get(Object arg0) {
-		keys.add(arg0);
-		ValueWrapper result = get(arg0);
-		if (result == null) {
-			keys.remove(arg0);
-		}
-		return result;
-	}
-
-	@Override
-	public void put(Object arg0, Object arg1) {
-		keys.add(arg0);
-		cache.put(arg0, arg1);
-	}
-
-	@Override
-	public ValueWrapper putIfAbsent(Object arg0, Object arg1) {
-		keys.add(arg0);
-		return cache.putIfAbsent(arg0, arg1);
-	}
-
-	@Override
-	public String getName() {
-		return cache.getName();
-	}
-
-	@Override
-	public Object getNativeCache() {
-		return cache.getNativeCache();
-	}
-
-}
